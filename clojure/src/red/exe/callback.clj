f7d43949683758680e5ddb352806b031773c90d6
kay <zhhkay@gmail.com>
2015-07-22 19:34:36 +0800

    finish red.exe.callback

--- clojure/src/red/exe/callback.clj
+++ clojure/src/red/exe/callback.clj
@@ -1,42 +1,20 @@
 (ns red.exe.callback
   (:require [thrift-clj.core :as thrift]
-            [thrift-clj.gen.services :as s]
-            [thrift-clj.transports :as t]
-            [thrift-clj.protocol.core :as proto])
-  (:import [org.apache.thrift.server
-            TServer TSimpleServer
-            TThreadPoolServer TNonblockingServer
-            TServer$Args TServer$AbstractServerArgs
-            TThreadPoolServer$Args TNonblockingServer$Args]
-           [org.apache.thrift.transport TServerSocket TNonblockingServerSocket]
-           [org.apache.thrift TProcessor]))
-
-
-;;thrift-clj的接口中将port置为零,自动获取一个随机可用端口,不可查.
-(defn- wrap-args
-  ^TServer$AbstractServerArgs
-  [^TServer$AbstractServerArgs base iface opts]
-  (let [opts (apply hash-map opts)
-        p (:protocol opts :compact)
-        proto (if (keyword? p)
-                (proto/protocol-factory p)
-                (apply proto/protocol-factory p))]
-    (doto base
-      (.protocolFactory proto)
-      (.processor (s/iface->processor iface)))))
-
-(defn- multi-threaded-server
-  ^TServer
-  [iface port & opts]
-  (let [t (t/blocking-server-transport port opts)]
-    {:server (TThreadPoolServer. (wrap-args (TThreadPoolServer$Args. t) iface opts))
-     :port (.. t getServerSocket getLocalPort)}))
-
+            [thrift-clj.transports :as t])
+  (:import [org.apache.thrift.server TThreadPoolServer TThreadPoolServer$Args]
+           [org.apache.thrift.transport TServerSocket]))
 
 (thrift/import
  (:types    [device.sdk.media MediaType MediaPackage])
  (:services [device.sdk.media Notify]))
 
+;;thrift-clj的接口中将port置为零,自动获取一个随机可用端口,不可查.
+(defonce wrap-args (deref #'thrift-clj.server/wrap-args))
+(defn- multi-threaded-server
+  [iface port & opts]
+  (let [t (t/blocking-server-transport port opts)]
+    {:server (TThreadPoolServer. (wrap-args (TThreadPoolServer$Args. t) iface opts))
+     :port (.. t getServerSocket getLocalPort)}))
 
 (defn- launched [])
 
@@ -45,24 +23,15 @@
     (try (thrift/stop! server)
          (catch Exception e (prn e)))))
 
-(defn- start-listen! [{:keys [device->connected device->offline device->media-finish device->media-data]}
-                      port]
+(defn start-listen! [{:keys [device->connected device->offline device->media-finish device->media-data]}]
   (let [handler (thrift/service Notify
                                 (Lanuched    [] (Lanuched))
                                 (Connected   [device-id] (device->connected device-id))
                                 (Offline     [device-id] (device->offline device-id))
                                 (MediaFinish [source-id] (device->media-finish source-id))
                                 (MediaData   [source-id media] (device->media-data source-id media)))
-        {:keys [server port]}  (multi-threaded-server handler port
+        {:keys [server port]}  (multi-threaded-server handler 0
                                                       :bind "localhost"
                                                       :protocol :compact)]
     (thrift/serve! server)
     {:closer (mk-stop-server server) :port port}))
-
-(defn try-to-start-listen [info]
-  (some (fn [port]
-          (try
-            (start-listen! info port)
-            (catch org.apache.thrift.transport.TTransportException e nil)
-            (catch Exception e (prn e))))
-        (range 50000 60000)))
