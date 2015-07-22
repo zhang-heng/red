647fae16ce8451cfcb7af1dcfbd2187e7158860f
kay <zhhkay@gmail.com>
2015-07-22 20:18:12 +0800

    finish red.exe.launcher

--- clojure/src/red/exe/launcher.clj
+++ clojure/src/red/exe/launcher.clj
@@ -1,11 +1,7 @@
 (ns red.exe.launcher
   (:require [environ.core :refer [env]])
   (:import [java.io
-            InputStreamReader BufferedReader
-            InputStream OutputStream FileInputStream DataInputStream
-            File
-            IOException EOFException]
-           [java.lang ProcessBuilder$Redirect]))
+            InputStreamReader BufferedReader File]))
 
 (defn- creat-process-builder [path command]
   (doto (ProcessBuilder. ^"[Ljava.lang.String;" command)
@@ -13,21 +9,26 @@
     (.. (environment)
         (put "LD_LIBRARY_PATH" path))))
 
-;; 启动进程
-(defn launch! [path name thrift-notify-port printer]
-  (try (let [command      (into-array String [(format "%s/%s.exe" path name) (str thrift-notify-port)])
-             proc-builder (creat-process-builder path command)
+(defn- mk-kill-process!
+  "杀进程"
+  [proc]
+  (fn []
+    (try
+      (.destroy proc)
+      (catch Exception e (prn e)))))
+
+(defn launch!
+  "启动进程
+  (launch! \"C:/Windows/system32/PING.exe\" \"C:/Windows/system32\" prn \"192.168.1.1\")"
+  [path working-path out-printer & args]
+  (try (let [command      (->> (apply conj [path] args)
+                               (into-array String))
+             proc-builder (creat-process-builder working-path command)
              proc         (.start proc-builder)
              br           (-> (.getInputStream proc) InputStreamReader. BufferedReader.)]
          (future
            (while (let [r (.readLine br)]
-                    (printer (str r))
+                    (out-printer (str r))
                     r)))
-         proc)
+         (mk-kill-process! proc))
        (catch Exception e (prn e))))
-
-;; 杀进程
-(defn exit-process! [{proc :process}]
-  (try
-    (.destroy proc)
-    (catch Exception e (prn e))))
