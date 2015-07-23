391af0ad079aaec62c2cb6354fb6f8affac5bb5a
zhangheng <zhhkay@gmail.com>
2015-07-23 11:38:05 +0800

    添加pid

--- clojure/src/red/exe/launcher.clj
+++ clojure/src/red/exe/launcher.clj
@@ -1,7 +1,5 @@
 (ns red.exe.launcher
-  (:require [environ.core :refer [env]])
-  (:import [java.io
-            InputStreamReader BufferedReader File]))
+  (:import [java.io InputStreamReader BufferedReader File]))
 
 (defn- creat-process-builder [path command]
   (doto (ProcessBuilder. ^"[Ljava.lang.String;" command)
@@ -17,18 +15,26 @@
       (.destroy proc)
       (catch Exception e (prn e)))))
 
+(defn- get-proc-pid [process]
+  (when-let [f (.. process getClass (getDeclaredField "pid"))]
+    (.setAccessible f true)
+    (long (.get f process))))
+
 (defn launch!
   "启动进程
-  (launch! \"C:/Windows/system32/PING.exe\" \"C:/Windows/system32\" prn \"192.168.1.1\")"
-  [path working-path out-printer & args]
-  (try (let [command      (->> (apply conj [path] args)
+  (launch! prn #(prn \"over\") \"ping\" \"/\" \"192.168.8.1\")"
+  [printer crasher path working-path & args]
+  (try (let [command      (->> (map str args)
+                               (apply conj [path])
                                (into-array String))
              proc-builder (creat-process-builder working-path command)
-             proc         (.start proc-builder)
-             br           (-> (.getInputStream proc) InputStreamReader. BufferedReader.)]
+             proc         (io! (.start proc-builder))
+             br           (-> (.getInputStream proc) InputStreamReader. BufferedReader.)
+             pid          (get-proc-pid proc)]
          (future
-           (while (let [r (.readLine br)]
-                    (out-printer (str r))
-                    r)))
+           (while (when-let [r (.readLine br)]
+                    (printer pid r)
+                    true))
+           (crasher))
          (mk-kill-process! proc))
        (catch Exception e (prn e))))
